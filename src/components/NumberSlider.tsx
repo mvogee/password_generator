5399389c286af5e4634eee50ae8efac2e6dabbff
Matthew Vogee <matthewvogee@Matthews-MacBook-Pro.local>
2022-06-20 20:09:47 -0700

    css and styling changes

--- src/components/NumberSlider.tsx
+++ src/components/NumberSlider.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/NumberSlider.css';
 
 type props = {
     passwordLength: number;
@@ -6,13 +7,21 @@ type props = {
 }
 
 const NumberSlider = ({ passwordLength, setPasswordLength } : props) => {
+
     const lengthChange = (event : React.FormEvent<HTMLInputElement>) => {
         setPasswordLength(parseInt(event.currentTarget.value));
+        let target = event.currentTarget;
+        const min = parseInt(target.min);
+        const max = parseInt(target.max);
+        const val = parseInt(target.value);
+        target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
     }
     return (
         <div className="numberSlider">
-            <p>Length: {passwordLength}</p>
-            <input type="range" max={100} min={8} defaultValue={15} onChange={lengthChange}></input>
+            <label>
+                Length: {passwordLength}
+            </label>
+                <input className="slider" type="range" max={64} min={8} defaultValue={15} onChange={lengthChange}></input>
         </div>
     );
 }
